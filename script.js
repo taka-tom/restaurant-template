356d15a41f5eb04fc26ff329b3fabb8fe9c830c8
taka-tom <takatomo.takatomo.01@gmail.com>
2026-09-02 13:52:55 +0900

    script.js を作成

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  // TEXT
+
+  document
+    .querySelectorAll("[data-site]")
+    .forEach((element) => {
+
+
+      const key = element.dataset.site;
+
+
+      if (SITE_CONFIG[key]) {
+
+        element.textContent =
+          SITE_CONFIG[key];
+
+      }
+
+
+    });
+
+
+  // COLOR
+
+  document.documentElement.style.setProperty(
+    "--main-color",
+    SITE_CONFIG.mainColor
+  );
+
+
+  document.documentElement.style.setProperty(
+    "--text-color",
+    SITE_CONFIG.textColor
+  );
+
+
+  document.documentElement.style.setProperty(
+    "--accent-color",
+    SITE_CONFIG.accentColor
+  );
+
+
+  // TITLE
+
+  document.title =
+    SITE_CONFIG.restaurantName;
+
+
+});
