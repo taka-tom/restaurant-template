9b8567a87a488b94a50f725cbd67668e3b8cdeaa
taka-tom <takatomo.takatomo.01@gmail.com>
2026-09-04 05:05:52 +0900

    config.js を更新

--- config.js
+++ config.js
@@ -9,6 +9,18 @@ const SITE_CONFIG = {
   menuImage02: "images/menu02.jpg",
   menuImage03: "images/menu03.jpg",
 
+  menuName01: "MENU 01",
+  menuDescription01: "MENU DESCRIPTION",
+  menuPrice01: "¥2,500",
+  
+  menuName02: "MENU 02",
+  menuDescription02: "MENU DESCRIPTION",
+  menuPrice02: "¥3,000",
+  
+  menuName03: "MENU 03",
+  menuDescription03: "MENU DESCRIPTION",
+  menuPrice03: "¥3,500",
+
 
   aboutText:
     "料理と空間を通して、大切な時間をお届けします。",
