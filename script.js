b40a4c5f904b112e4db6a95550661dab5dd7015a
taka-tom <takatomo.takatomo.01@gmail.com>
2026-09-05 11:08:14 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -79,4 +79,20 @@ document
 
   });
   
+  const menuToggle =
+  document.querySelector(".menu-toggle");
+
+const nav =
+  document.querySelector("nav");
+
+
+menuToggle.addEventListener(
+  "click",
+  () => {
+
+    nav.classList.toggle("active");
+
+  }
+);
+  
   });
